c1c8e03bf76f4c7404d93b62a63eb3d6827ec3c9
cyberz3r0 <reidsl0716@gmail.com>
2023-09-19 15:57:48 -0700

    redirected if user is logged in

--- password_gen_app/controllers/passwords_controller.py
+++ password_gen_app/controllers/passwords_controller.py
@@ -7,6 +7,8 @@ from ..models.user import User
 
 @app.route('/')
 def index():
+    if 'user_logged_id' in session:
+        redirect("/logged_in")
     
     if 'generated_password' in session:
         generated_password = session['generated_password']
